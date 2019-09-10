fa5dcefd8fda6a9b0e2e338454c4400e30bb0601
eduhcavalcanti <eduh.cavalcanti@hotmail.com>
2019-09-10 16:43:06 -0300

    Alguns comentários

--- src/controllers/controllerAllUserLogin.js
+++ src/controllers/controllerAllUserLogin.js
@@ -1,12 +1,13 @@
 const userModel = require('../model/modelSchema')
 
 module.exports = {
+  //Usa "create" para criar um usuários
   async resgiter(req, res) {
     const user = await userModel.create(req.body)
 
     return res.json(user)
   },
-
+//Usa o find para mostrar todos os ussários cadastrados
   async all(req, res) {
     const allUser = await userModel.find()
 
@@ -14,6 +15,5 @@ module.exports = {
   }
 
 }
-
 
 
